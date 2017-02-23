56f4aba0c113f1c4a944b3db1b2f2ce21cda4ead
HighX97 <loic.ortole@gmail.com>
2017-02-23 15:26:10 +0100

    Menu Collant

--- Promise_Async_Await/js/MenuCollant/app.js
+++ Promise_Async_Await/js/MenuCollant/app.js
@@ -20,6 +20,17 @@
       console.log("element callback",element);
       //Lorsqu'on appel plusieur fois la meme fonction il est plus performant de la stocker au préalable dans une variable
       var rect = element.getBoundingClientRect()
+      //if element.getAttribute('data-offset') === null then 0 else value
+      var offset = parseInt(element.getAttribute('data-offset') || 0, 10)
+      console.log("offset",offset);
+      if (element.getAttribute('data-constraint')) {
+        var constraint = document.querySelector(element.getAttribute('data-constraint'))
+      }
+      else {
+        var constraint = document.body
+      }
+      var constraintRect = constraint.getBoundingClientRect()
+      var constraintBottom = constraintRect.top + scrollY() + constraintRect.height - offset - rect.height
       var top = rect.top +scrollY()
       var fake = document.createElement('div')
       fake.style.width = rect.width + "px"
@@ -30,26 +41,40 @@
       var onScroll =  function () {
         var hasScrollClass = element.classList.contains('fixed')
         console.log('scroll');
-        if (scrollY() > top && !hasScrollClass ) {
+
+        if (scrollY() > constraintBottom && element.style.position != 'absolute') {
+          element.classList.remove('fixed')
+          element.style.position = 'absolute'
+          element.style.bottom = '0'
+          element.style.top = 'auto'
+        } else if (scrollY() > (top - offset) && scrollY() < constraintBottom && !hasScrollClass && element.style.position != 'fixed') {
           console.log("< 0");
           element.classList.add('fixed')
+          element.style.bottom = 'auto'
+          element.style.position = 'fixed'
+          element.style.top= offset + "px"
           element.style.width = rect.width
           element.parentNode.insertBefore(fake, element)
         }
-        else if (scrollY() < top && hasScrollClass ) {
+        else if (scrollY() < (top - offset) && hasScrollClass  && element.style.position != 'static') {
           console.log(">= 0");
           element.classList.remove('fixed')
-          element.parentNode.removeChild(fake)
-
+          element.style.position = 'static'
+          if (element.parentNode.contains(fake)){
+            element.parentNode.removeChild(fake)
+          }
         }
       }
 
       var onResize = function(){
         console.log("onResize");
-        element.style.width = "auto"
+        element.style.width = 'auto'
         element.classList.remove('fixed')
+        element.style.position = 'static'
         fake.style.display = "none"
         rect = element.getBoundingClientRect()
+        constraintRect = constraint.getBoundingClientRect()
+        constraintBottom = constraintRect.top + scrollY() + constraintRect.height - offset - rect.height
         top = rect.top +scrollY()
         fake.style.width = rect.width + "px"
         fake.style.height = rect.height + "px"
